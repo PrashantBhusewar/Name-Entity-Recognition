e27dba9c0546222c977f399b72a8604f062d7396
PrashantBhusewar <prashantbhusewar5@gmail.com>
2024-04-08 15:38:26 +0530

    data ingestion

--- train.py
+++ train.py
@@ -1,4 +1,18 @@
-from ner.configuration.gcloud import GCloud
+import os
+import sys
 
-obj = GCloud()
-obj.sync_folder_from_gcloud(gcp_bucket_url="ner-using-bert-15", filename="archive.zip", destination="test")
+from ner.exception import NerException
+from ner.pipeline.train_pipeline import TrainPipeline
+from ner.constants import *
+
+
+def training():
+    try:
+        train_pipeline = TrainPipeline()
+        train_pipeline.run_pipeline()
+
+    except Exception as e:
+        raise NerException(e, sys) from e
+    
+if __name__ == '__main__':
+    training()
