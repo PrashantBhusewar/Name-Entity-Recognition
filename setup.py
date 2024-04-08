ab49532d2dbd2c008fa40246248e44b11d4ff97c
PrashantBhusewar <prashantbhusewar5@gmail.com>
2024-04-08 13:37:12 +0530

    requirements added

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="Name-Entity-Recognition",
+    name="ner-using-bert",
     version="0.0.1",
     author="Prashant",
     author_email="prashantbhusewar1599@gmail.com",
